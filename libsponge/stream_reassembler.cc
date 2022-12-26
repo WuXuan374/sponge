e25e9de9cc7b625f6063e4554f5d0f2f85184eea
WuXuan374 <2953098003@qq.com>
2022-12-26 12:34:49 +0000

    fix: debug

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -1,4 +1,5 @@
 #include "stream_reassembler.hh"
+#include <iostream>
 
 // Dummy implementation of a stream reassembler.
 
@@ -10,7 +11,6 @@
 template <typename... Targs>
 void DUMMY_CODE(Targs &&... /* unused */) {}
 
-// using namespace std;
 /**
  * 这三个变量是始终要记得维护的
  * _idx_to_substring
@@ -18,71 +18,108 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
  * _unassembled_count
 */
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity), _idx_to_substring({}), _assembled_end_index(0), _unassembled_count(0), _eof_index(SIZE_MAX) {}
+StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity), _idx_to_string(), _assembled_end_index(0), _unassembled_count(0), _eof_index(SIZE_MAX) {}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const std::string &data, const size_t index, const bool eof) {
-    // 首先进行 capacity 的检查
-    size_t current_bytes = bytes_count();
-    if (current_bytes >= _capacity) {
-        return; // 这个 string 直接被舍弃
-    }
-    // 无论是写入 stream 还是留在 unassembled 存储中，能够写入的最大长度
-    size_t bytes_left = _capacity - current_bytes;
-    // 能不能直接写入 byte stream 中
-    if (index <= _assembled_end_index) { // 可能存在 overlap
-        // 超出 capacity 的，同样是直接被舍弃
-        if (_assembled_end_index-index < data.length()) {
-            string written_data = data.substr(_assembled_end_index-index, bytes_left); 
-            _output.write(written_data);
-            _assembled_end_index += written_data.length();
-            // 检查是否有之前的数据，能够被重组
-            assemble_string();
-            check_eof();
-        }
-    } else {
-        string written_data = data.substr(0, bytes_left); 
-        // 没有新数据写入 byte stream 中，那么已有的 unassembled string 肯定不发生变化
-        _idx_to_substring[index] = written_data;
-        _unassembled_count += written_data.length();
-    }
     if (eof) {
         _eof_index = index + data.length();
         check_eof();
     }
+    // 分成两步走，先把 string 写入队列
+    // 然后统一检查队列中有哪些 bytes 可以被重组
+    push_string_to_queue(data, index);
+    assemble_string_from_queue();
 }
 
-void StreamReassembler::assemble_string() {
-    // 尝试将 unassembled string 写入 stream 中
-    while (true) {
-        // 将 unassembled string 写入 stream 中, 并不影响 Reassembler 的容量
-        // 但是受限于 ByteStream 剩余的容量
-        size_t bytes_left = _output.remaining_capacity();
-        if (bytes_left <= 0) return;
-        
-        auto search = _idx_to_substring.find(_assembled_end_index);
-        if (search == _idx_to_substring.end()) return;
+void StreamReassembler::push_string_to_queue(const std::string &data, const size_t index) {
+    /**
+     * 把接收的 string 写入 queue 中
+     * 只有索引 >= _assembled_end_index 的部分，才有必要写入
+     * 写入 queue 的时候，要注意 capacity 限制 
+     * 修改状态: _unassembled_count
+    */
 
-        // 找到了 <key, value> pair, 可以写入 bytes_left 个 byte
-        size_t start_idx = search -> first;
-        string data = search -> second;
-        string written_data = data.substr(0, bytes_left);
+    size_t current_bytes = bytes_count();
+    if (current_bytes >= _capacity) {
+        return; // 这个 string 直接被舍弃
+    }
+    // 能够写入的最大长度
+    size_t bytes_left = _capacity - current_bytes;
 
-        _output.write(written_data);
-        _assembled_end_index += written_data.length();
-        _unassembled_count -= written_data.length();
-        // 如果这个 string 没有全部写完
-        if (written_data.length() < data.length()) {
-            // substr, 第二个参数默认为 npos, 则返回 [pos, size()]
-            _idx_to_substring[start_idx+written_data.length()] = data.substr(written_data.length());
-        }
-        check_eof();
+    const size_t start_idx = _assembled_end_index > index ? (_assembled_end_index - index) : 0;
+    if (start_idx >= data.length()) { // 空字符串也没有存储的必要
+        return;
+    }
+
+    // 注意 substr 的 start_idx, 是相对于这个字符串而言的，不是整体的 index
+    const std::string written_data = data.substr(start_idx, bytes_left);
+    if (written_data.length() > 0) {
+        _idx_to_string.push(
+            make_pair(
+                index + start_idx, 
+                written_data
+            )
+        );
+        _unassembled_count += written_data.length();
     }
 }
 
+
+void StreamReassembler::assemble_string_from_queue() {
+    /**
+     * 遍历 _idx_to_substring, 依次将 string 写入 byte stream 中
+     * 这里头我们就不需要考虑 capacity 了，本操作不影响 capacity
+     * 需要维护的状态: _unassembled_count
+    */
+    while (!_idx_to_string.empty()) {
+        const size_t cur_index = _idx_to_string.top().first;
+        const std::string cur_data = _idx_to_string.top().second;
+        cout << "80: " << cur_index << " " << _assembled_end_index << endl;
+        
+        if (cur_index > _assembled_end_index) {
+            break; // 队列中不存在能够重组的字符串
+        }
+        _idx_to_string.pop();
+        _unassembled_count -= cur_data.length();
+        assemble_string_to_stream(cur_data, cur_index);
+
+        // 根据 _assembled_end_index 的值，我们可以记录有多少 byte 被写入，并相应处理
+        if (_assembled_end_index < cur_index + cur_data.length()) {
+            // string 没有全部被写入
+            // cout << "91: " << _assembled_end_index << p.first << " " << _assembled_end_index - p.first << " " << p.second.length() << endl;
+            const std::string string_left = cur_data.substr(_assembled_end_index - cur_index);
+            _idx_to_string.push(make_pair(_assembled_end_index, string_left));
+            _unassembled_count += string_left.length();
+        }
+    }
+}
+
+void StreamReassembler::assemble_string_to_stream(const std::string &data, const size_t index) {
+    /**
+     * 将 string 写入 byte stream 中
+     * 这一步不需要考虑 capacity, 调用时会保证
+    */
+
+    // data 的所有 bytes 都已经写入 byte stream 了
+    if (_assembled_end_index - index >= data.length()) {
+        return;
+    }
+    const std::string written_data = data.substr(_assembled_end_index - index, _output.remaining_capacity());
+    _output.write(written_data);
+    // 修改状态
+    _assembled_end_index += written_data.length();
+    check_eof();
+}
+
 void StreamReassembler::check_eof() {
+    /**
+     * _assembled_end_index 发生变化
+     * 或者 _eof_index 发生变化
+     * 就应该调用此函数
+    */
     if (_eof_index != SIZE_MAX) {
         if (_assembled_end_index == _eof_index) {
             _output.end_input();
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -5,7 +5,20 @@
 
 #include <cstdint>
 #include <string>
-#include <unordered_map>
+#include <queue>
+
+// class ComparisonClass {
+//   public:
+//       bool operator() (pair<size_t, std::string> const& a, pair<size_t, std::string> const& b) {
+//           return a.first < b.first;
+//       }
+//   };
+
+struct CustomCompare {
+  bool operator()(const pair<size_t, std::string>& a, const pair<size_t, std::string>& b) {
+    return a.first > b.first;
+  }
+};
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
@@ -15,12 +28,28 @@ class StreamReassembler {
 
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
-    unordered_map<int, std::string> _idx_to_substring; // index 到 substring 的映射（还没有重组的数据）
+    
+    // idx 到 string 的映射，按照 idx 升序存储
+    priority_queue< pair<size_t, std::string>, std::vector<pair<size_t, std::string>>, CustomCompare > _idx_to_string;
+    // priority_queue<pair<size_t, std::string>> _idx_to_string;
     size_t _assembled_end_index; // 已经重组好的数据的 end index, 注意是开区间
     // unassembled string 长度
     // 每次新增 unassembled string, 或者有 string 被写入 stream 的时候，要记得修改
     size_t _unassembled_count; 
     size_t _eof_index;
+
+    // Byte Stream unread length + unassembled string length
+    size_t bytes_count() const;
+
+    void push_string_to_queue(const std::string &data, const size_t index);
+
+    // 把 string 写入 byte stream, 这一个函数内不需要考虑 capacity
+    void assemble_string_to_stream(const std::string &data, const size_t index);
+
+    // 组合之前没有重组成功，存储在优先级队列中的 string
+    void assemble_string_from_queue();
+
+    void check_eof();
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
@@ -53,13 +82,6 @@ class StreamReassembler {
     //! \brief Is the internal state empty (other than the output stream)?
     //! \returns `true` if no substrings are waiting to be assembled
     bool empty() const;
-
-    // Byte Stream unread length + unassembled string length
-    size_t bytes_count() const;
-
-    void assemble_string();
-
-    void check_eof();
 };
 
 #endif  // SPONGE_LIBSPONGE_STREAM_REASSEMBLER_HH
