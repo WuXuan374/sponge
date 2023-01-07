facc005845126b619e1d29702a6494a22cf24b46
WuXuan374 <xuanwu@smail.nju.edu.cn>
2023-01-07 13:37:34 +0000

    feat: finish wrap & unwrap

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,5 +1,4 @@
 #include "wrapping_integers.hh"
-
 // Dummy implementation of a 32-bit wrapping integer
 
 // For Lab 2, please replace with a real implementation that passes the
@@ -10,11 +9,13 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
+uint64_t RANGE = 1ul << 32;
+
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    uint32_t remainder = (isn + n).raw_value() % UINT32_MAX;
+    uint32_t remainder = (n + isn.raw_value()) % RANGE;
     return WrappingInt32{remainder};
 }
 
@@ -29,16 +30,17 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    uint64_t absolute; // [0, UINT32_MAX]
+    uint64_t absolute;
     if (n.raw_value() < isn.raw_value()) {
-        absolute = n.raw_value() + UINT32_MAX - isn.raw_value();
+        absolute = n.raw_value() + RANGE - isn.raw_value();
     } else {
         absolute = n.raw_value() - isn.raw_value();
+    } // absolute: [0, RANGE-1]
+    if (checkpoint > absolute) {
+        absolute += RANGE * ((checkpoint - absolute) / RANGE);
+        if ((checkpoint - absolute) % RANGE > RANGE / 2) {
+            absolute += RANGE;
+        }
     }
-    
-    while (checkpoint > absolute && checkpoint - absolute > (UINT32_MAX / 2)) {
-        absolute += UINT32_MAX;
-    }
-
     return absolute;
 }
